b5ed1f4ee2f0312c26d6305edd6854676b37f73c
Tomáš Vaněk <tomas.vanek@vancloud.cz>
2023-12-05 21:28:08 +0100

    přidány základní vlastnosti pro ASS formát

--- SubtitleBlocks/AssSubtitleBlock.cs
+++ SubtitleBlocks/AssSubtitleBlock.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OpenSubtitles.SubtitleBlocks
+﻿namespace OpenSubtitles.SubtitleBlocks
 {
-    public class AssSubtitleBlock
+    public class AssSubtitleBlock : SubtitleBlockBase
     {
+        // Základní vlastnosti ASS titulku
+        public int Layer { get; set; }
+        public string Style { get; set; }
+        public string Name { get; set; }
+        public string MarginL { get; set; }
+        public string MarginR { get; set; }
+        public string MarginV { get; set; }
+        public string Effect { get; set; }
+
+        // ASS specifické vlastnosti pro animace a efekty
+        // Tyto vlastnosti mohou obsahovat informace o animacích, pohybu, blikání, atd.
+        public string Animation { get; set; }
+
+        // Další vlastnosti
+        // Můžete zde přidat další vlastnosti specifické pro ASS, například karaoke efekty, pokud je to relevantní pro vaši aplikaci
+
+        public AssSubtitleBlock()
+        {
+            // Inicializační logika
+        }
+
+        // Metody pro specifické zpracování ASS titulků
+        // Například metoda pro zpracování ASS tagů v textu nebo pro převod mezi formáty
     }
 }
+
