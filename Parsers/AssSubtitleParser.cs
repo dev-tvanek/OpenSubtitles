4bc9ab437c90d95603a445c18fcc456dd93b1203
Tomáš Vaněk <tomas.vanek@vancloud.cz>
2023-12-05 16:37:32 +0100

    Vytvoření logiky pro ASS a VTT

--- Parsers/AssSubtitleParser.cs
+++ Parsers/AssSubtitleParser.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace OpenSubtitles.Parsers
 {
@@ -10,7 +9,66 @@ namespace OpenSubtitles.Parsers
     {
         public override List<SubtitleBlock> Parse(string filePath)
         {
-            return [];
+            var subtitles = new List<SubtitleBlock>();
+            var lines = File.ReadAllLines(filePath);
+            bool isEventsSection = false;
+
+            foreach (var line in lines)
+            {
+                // Kontrola, zda jsme v sekci událostí (Events)
+                if (line.StartsWith("[Events]"))
+                {
+                    isEventsSection = true;
+                    continue;
+                }
+
+                // Parsování titulků v sekci událostí
+                if (isEventsSection && line.StartsWith("Dialogue:"))
+                {
+                    var subtitle = ParseDialogueLine(line);
+                    if (subtitle != null)
+                    {
+                        subtitles.Add(subtitle);
+                    }
+                }
+            }
+
+            return subtitles;
+        }
+
+        private SubtitleBlock ParseDialogueLine(string line)
+        {
+            // ASS Dialogue line format: Dialogue: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
+            var parts = line.Split(',');
+
+            if (parts.Length < 10)
+            {
+                return null; // Neplatný formát řádku
+            }
+
+            var startTime = ParseAssTime(parts[1].Trim());
+            var endTime = ParseAssTime(parts[2].Trim());
+            var text = string.Join(",", parts.Skip(9)); // Text může obsahovat další čárky
+
+            return new SubtitleBlock
+            {
+                StartTime = startTime,
+                EndTime = endTime,
+                Text = ProcessHtmlTags(text) // Předpokládá se, že HTML značky mohou být v textu
+            };
+        }
+
+        private TimeSpan ParseAssTime(string timeString)
+        {
+            // Formát času v ASS: h:mm:ss.cc (hodiny:minuty:sekundy.centisekundy)
+            var parts = timeString.Split(':');
+            var hours = int.Parse(parts[0]);
+            var minutes = int.Parse(parts[1]);
+            var seconds = int.Parse(parts[2].Split('.')[0]);
+            var centiseconds = int.Parse(parts[2].Split('.')[1]);
+
+            return new TimeSpan(0, hours, minutes, seconds, centiseconds * 10);
         }
     }
 }
+
--- Parsers/VttSubtitleParser.cs
+++ Parsers/VttSubtitleParser.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace OpenSubtitles.Parsers
 {
@@ -10,8 +10,51 @@ namespace OpenSubtitles.Parsers
     {
         public override List<SubtitleBlock> Parse(string filePath)
         {
-            return [];
+            var subtitles = new List<SubtitleBlock>();
+            var lines = File.ReadAllLines(filePath);
+            SubtitleBlock currentSubtitle = null;
+
+            foreach (var line in lines)
+            {
+                // Kontrola a zpracování časových značek
+                if (line.Contains("-->"))
+                {
+                    var times = line.Split(new[] { " --> " }, StringSplitOptions.None);
+                    currentSubtitle = new SubtitleBlock
+                    {
+                        StartTime = ParseVttTime(times[0].Trim()),
+                        EndTime = ParseVttTime(times[1].Trim())
+                    };
+                }
+                else if (!string.IsNullOrWhiteSpace(line) && currentSubtitle != null)
+                {
+                    if (currentSubtitle.Text == null)
+                        currentSubtitle.Text = line;
+                    else
+                        currentSubtitle.Text += "\n" + line;
+                }
+                else if (string.IsNullOrWhiteSpace(line) && currentSubtitle != null)
+                {
+                    subtitles.Add(currentSubtitle);
+                    currentSubtitle = null;
+                }
+            }
+
+            // Přidání posledního titulku, pokud soubor nekončí prázdným řádkem
+            if (currentSubtitle != null)
+            {
+                subtitles.Add(currentSubtitle);
+            }
+
+            return subtitles;
+        }
+
+        private TimeSpan ParseVttTime(string timeString)
+        {
+            // Formát času ve VTT: HH:MM:SS.MMM
+            return TimeSpan.ParseExact(timeString, @"hh\:mm\:ss\.fff", CultureInfo.InvariantCulture);
         }
     }
 }
+
 
